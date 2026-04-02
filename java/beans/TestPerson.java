fcff3914c8549e867fcb79922a641068fa5d0b12
Pranjay <pranjaychanotia@gmail.com>
2026-04-02 23:18:45 +0530

    Added JavaBeans Part 1

---
+++ java/beans/TestPerson.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+
+        Person p = new Person();
+
+        p.setName("Pranjay");
+        p.setAge(21);
+
+        System.out.println("Name: " + p.getName());
+        System.out.println("Age: " + p.getAge());
+    }
+}
